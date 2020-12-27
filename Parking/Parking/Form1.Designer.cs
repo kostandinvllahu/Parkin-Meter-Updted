b3d20d93c63e8e0281913784fff752e75b759fcf
Kostandin Vllahu <33177557+kostandinvllahu@users.noreply.github.com>
2020-12-27 13:57:34 +0100

    Add files via upload
    More features created and some bug fixed.

---
+++ Parking/Parking/Form1.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace Parking
+{
+    partial class ParkingMeter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.Submit = new System.Windows.Forms.Button();
+            this.tekst1 = new System.Windows.Forms.TextBox();
+            this.Reset = new System.Windows.Forms.Button();
+            this.input1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Option = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Submit
+            // 
+            this.Submit.BackColor = System.Drawing.Color.SpringGreen;
+            this.Submit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Submit.Location = new System.Drawing.Point(62, 379);
+            this.Submit.Name = "Submit";
+            this.Submit.Size = new System.Drawing.Size(103, 27);
+            this.Submit.TabIndex = 2;
+            this.Submit.Text = "PAY";
+            this.Submit.UseVisualStyleBackColor = false;
+            this.Submit.Click += new System.EventHandler(this.Submit_Click);
+            // 
+            // tekst1
+            // 
+            this.tekst1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tekst1.Location = new System.Drawing.Point(524, 218);
+            this.tekst1.Name = "tekst1";
+            this.tekst1.ReadOnly = true;
+            this.tekst1.Size = new System.Drawing.Size(100, 20);
+            this.tekst1.TabIndex = 3;
+            this.tekst1.TextChanged += new System.EventHandler(this.tekst1_TextChanged);
+            // 
+            // Reset
+            // 
+            this.Reset.BackColor = System.Drawing.Color.Gold;
+            this.Reset.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Reset.Location = new System.Drawing.Point(526, 379);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(103, 27);
+            this.Reset.TabIndex = 4;
+            this.Reset.Text = "RESET";
+            this.Reset.UseVisualStyleBackColor = false;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // input1
+            // 
+            this.input1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.input1.Location = new System.Drawing.Point(62, 218);
+            this.input1.Name = "input1";
+            this.input1.Size = new System.Drawing.Size(100, 20);
+            this.input1.TabIndex = 5;
+            this.input1.TextChanged += new System.EventHandler(this.input1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(236, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 31);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "PARKING METER";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(91, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(533, 24);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "DO YOU WANT TO STAY MORE OR LESS THEN 24H ?\r\n";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(62, 122);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(121, 17);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "LESS THEN 24H";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(524, 122);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(126, 17);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "MORE THEN 24H";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(212, 218);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(276, 68);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.Location = new System.Drawing.Point(212, 338);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(276, 68);
+            this.richTextBox2.TabIndex = 11;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(293, 302);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 24);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "TICKET ID";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(293, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 24);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "PRICES";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(58, 175);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 24);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "HOURS";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(520, 175);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(109, 24);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "PAYMENT";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(297, 428);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 27);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "PRINT TICKET";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Option
+            // 
+            this.Option.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Option.Location = new System.Drawing.Point(62, 244);
+            this.Option.Name = "Option";
+            this.Option.ReadOnly = true;
+            this.Option.Size = new System.Drawing.Size(100, 20);
+            this.Option.TabIndex = 17;
+            this.Option.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(526, 255);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 18;
+            this.textBox1.Visible = false;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(750, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(38, 23);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(62, 302);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 20;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(58, 275);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 24);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "DAYS";
+            // 
+            // ParkingMeter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 467);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Option);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.input1);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.tekst1);
+            this.Controls.Add(this.Submit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ParkingMeter";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Parking Meter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button Submit;
+        private System.Windows.Forms.TextBox tekst1;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.TextBox input1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox RichTextBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Option;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Parking/Parking/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parking
+{
+    public partial class ParkingMeter : Form
+    {
+        public ParkingMeter()
+        {
+            InitializeComponent();
+        }
+        int charge;
+        double time;
+        //============DHE KETU=======================
+        Random r = new Random();
+        int sum = 0;
+        /*private void btn_Click(object sender, EventArgs e)
+        {
+            sum += r.Next(0, 100);
+            richTextBox2.Text = sum.ToString();
+        }*/
+        //================TEST KETU===================
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Clean()
+        {
+            tekst1.Text = "";
+            input1.Text = "";
+            richTextBox1.Text = "";
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            richTextBox2.Text = "";
+            Option.Text = "";
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+
+
+            private void tekst1_TextChanged(object sender, EventArgs e)
+        {
+            if(radioButton1.Checked == true)
+            {
+                if(charge >= 24)
+                {
+                    tekst1.Text = " ";
+                    
+                }
+                }
+            if (radioButton2.Checked == true)
+            {
+               
+                if (charge <= 23)
+                {
+                    tekst1.Text = " ";
+                    
+                }
+            }
+            }
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == false && radioButton2.Checked == false)
+            {
+                MessageBox.Show("Choose an option!");
+            }
+
+            else
+            {
+                charge = Convert.ToInt32(input1.Text);
+
+                if (charge <= 3 || charge == 0)
+                {
+                    tekst1.Text = "2$";
+                    int a = Convert.ToInt32(input1.Text);
+                    int n = Convert.ToInt32(DateTime.Now.ToString("hh"));
+                    int c = n + a;
+                    textBox1.Text = Convert.ToString(c) + ":" + Convert.ToInt32(DateTime.Now.ToString("mm"));
+                    richTextBox2.Text = "========PARKING TICKET========= " +
+                       "\n DATE: " + DateTime.Now.ToString("dddd , MMM dd yyyy,hh:mm:ss") +
+                    "\n END DATE: " + textBox1.Text +
+                   " \nTICKET ID. " + " AA" + sum.ToString() +
+                       "\nOPTION: " + Option.Text +
+                       "\nHOURS: " + input1.Text +
+                       "\nPRICE: " + tekst1.Text +
+                       " \nTHANK YOU FOR USING OUR SERVICE";
+                }
+                else
+                {
+                    time = 2;
+                    for (int i = 3; i < charge; i++)
+                    {
+                        time += 0.50;
+                        tekst1.Text = time.ToString() + "$";
+                        int a = Convert.ToInt32(input1.Text);
+                        int n = Convert.ToInt32(DateTime.Now.ToString("hh"));
+                        int c = n + a;
+                        textBox1.Text = Convert.ToString(c) + ":" + Convert.ToInt32(DateTime.Now.ToString("mm")); ;
+                    }
+                    sum += r.Next(0, 100);
+                    richTextBox2.Text = "========PARKING TICKET========= " +
+                        "\n DATE: " + DateTime.Now.ToString("dddd , MMM dd yyyy,hh:mm:ss") +
+                     "\n END DATE: " + textBox1.Text +
+                    " \nTICKET ID. " + " AA" + sum.ToString() +
+                        "\nOPTION: " + Option.Text +
+                        "\nHOURS: " + input1.Text +
+                        "\nPRICE: " + tekst1.Text +
+                        " \nTHANK YOU FOR USING OUR SERVICE";
+                    //===============KETU TESTIM==================
+                    if (radioButton1.Checked == true)
+                    {
+                        if (charge >= 24)
+                        {
+                            MessageBox.Show("Choose the option More then 24h");
+                            Clean();
+                        }
+                    }
+                    else
+                    {
+                        if (radioButton2.Checked == true)
+                        {
+                            if (charge <= 23)
+                            {
+                                MessageBox.Show("CHOOSE LESS THEN 24H");
+                                Clean();
+                            }
+                        }
+                    }
+                }
+            }
+                int num = 24;
+                int z = charge / num;
+                textBox2.Text = Convert.ToString(z + "Days");
+            }
+
+
+
+
+
+
+            private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            Clean();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+           if(radioButton1.Checked == true)
+            {
+               
+                richTextBox1.Text = "Price for less than 24h\n" +
+                    "0-3h = 2$\n" +
+                    "3-23h = 2$ + 0.5$/h";
+
+                
+            }
+           if(radioButton1.Checked == true)
+            {
+                Option.Text = "Less then 24h";
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if(radioButton2.Checked == true)
+            {
+                richTextBox1.Text = "Price for more than 24h\n" +
+                    "1 day = 10$\n" +
+                    "3-5 days = 35$\n" +
+                    "5+ days = 35$ + 5$";
+            }
+
+            if(radioButton2.Checked == true)
+            {
+                Option.Text = "More then 24h";
+            }
+
+        }
+
+        private void input1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text == "")
+            {
+                MessageBox.Show("Please choose an option and input the hours!");
+            }
+            else
+            {
+
+                MessageBox.Show("========PARKING TICKET========= " +
+                        "\n START DATE: " + DateTime.Now.ToString("dddd , MMM dd yyyy,hh:mm:ss") +
+                      "\n END DATE: " + textBox1.Text + 
+                " \nTICKET ID. " + " AA" + sum.ToString() +
+                        "\nOPTION: " + Option.Text +
+                        "\nHOURS: " + input1.Text +
+                        "\nPRICE: " + tekst1.Text +
+                        " \nTHANK YOU FOR USING OUR SERVICE") ;
+
+                Clean();
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        //PER TE PARE DITET
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
+
